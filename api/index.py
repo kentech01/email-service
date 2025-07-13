1170f42042c5ea1fec8a8043ddcd338f35e16839
berat-ademi <berat.ademi@gmail.com>
2025-07-13 13:19:28 +0200

    Add Mangum handler and configure vercel.json for FastAPI deployment on Vercel

---
+++ api/index.py
@@ -0,0 +1,5 @@
+from send_email import app  # Import your FastAPI app instance
+
+from mangum import Mangum  # Adapter for ASGI apps to AWS Lambda-like environments (works on Vercel)
+
+handler = Mangum(app)
